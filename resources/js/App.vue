47f22cabb5837dc4f7bea4ce5f9eaeae06d30bf8
Nader <nader.davarmanesh62@gmail.com>
2026-09-08 11:25:57 +0330

    Set up Vue and Tailwind

---
+++ resources/js/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="min-h-screen bg-red-500 flex items-center justify-center">
+        <h1 class="text-5xl font-bold text-white">
+            ROYA TEST
+        </h1>
+    </div>
+</template>
